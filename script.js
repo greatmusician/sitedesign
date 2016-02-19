287ef24d4b730cdfcade9bc4e4bc46ac3cd244b9
greatmusician <cwims29@gmail.com>
2016-02-19 05:24:24 -0500

    Added files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='war.html'>WAR</a></li>" +
+"<li><a href='biweekly.html'>Bi-Weekly</a></li>" +
+"</ul>";
+
+
+function myFunction() {
+    alert("Please Choose Your Role");
+}
